758673a98f16e53947d626ece120ef955c95a17b
jws <jws2920@naver.com>
2025-06-18 08:57:47 +0900

    [Refactor] 내부 변수 이름 통일화(addr -> lba)

--- src/write_read_aging.py
+++ src/write_read_aging.py
@@ -1,8 +1,8 @@
 import random
 from src.command_action import CommandAction
 
-TEST_ADDRESS_1 = 0
-TEST_ADDRESS_2 = 99
+TEST_LBA_1 = 0
+TEST_LBA_2 = 99
 
 
 class WriteReadAging(CommandAction):
@@ -14,7 +14,7 @@ class WriteReadAging(CommandAction):
     def run(self) -> None:
         for _ in range(200):
             self.write_data()
-            if not self.read_compare(TEST_ADDRESS_1, TEST_ADDRESS_2):
+            if not self.read_compare(TEST_LBA_1, TEST_LBA_2):
                 print("FAIL")
                 return
 
@@ -23,8 +23,8 @@ class WriteReadAging(CommandAction):
 
     def write_data(self):
         test_value = f"0x{random.randint(1111111, 4444444):08X}"
-        self._ssd_driver.write(TEST_ADDRESS_1, test_value)
-        self._ssd_driver.write(TEST_ADDRESS_2, test_value)
+        self._ssd_driver.write(TEST_LBA_1, test_value)
+        self._ssd_driver.write(TEST_LBA_2, test_value)
 
-    def read_compare(self, addr1, addr2) -> bool:
-        return self._ssd_driver.read(addr1) == self._ssd_driver.read(addr2)
+    def read_compare(self, lba1, lba2) -> bool:
+        return self._ssd_driver.read(lba1) == self._ssd_driver.read(lba2)
