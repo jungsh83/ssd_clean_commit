2799c39ab8df5b3da6ecca29af13c75e499cfe15
jungsh83 <jungsh83@gmail.com>
2025-06-17 15:49:55 +0900

    [feature] test_partial_lba_write_name_클래스변수_리스트_확인 케이스 추가 (green)

--- src/partial_lba_write.py
+++ src/partial_lba_write.py
@@ -2,6 +2,7 @@ from src.command_action import CommandAction
 
 
 class PartialLBAWrite(CommandAction):
+    command_name = ['2_PartialLBAWrite', '2_']
 
     def run(self) -> None:
         pass
--- tests/test_partial_lba_write.py
+++ tests/test_partial_lba_write.py
@@ -6,3 +6,6 @@ from src.partial_lba_write import PartialLBAWrite
 
 def test_partial_lba_write_validate_호출_정상(mocker: MockerFixture):
     assert PartialLBAWrite(ssd_driver=mocker.Mock()).validate()
+
+def test_partial_lba_write_name_클래스변수_리스트_확인(mocker: MockerFixture):
+    assert PartialLBAWrite.command_name == ['2_PartialLBAWrite', '2_']
