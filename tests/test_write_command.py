008fe6ca0b0f0aea1b1491f315ba22012aac667b
jihun75.kim <gnswlrla0218@naver.com>
2025-06-17 15:57:49 +0900

    [Refactor] write_command value 유효성 체크 기능 일반화 refactoring

--- tests/test_write_command.py
+++ tests/test_write_command.py
@@ -31,9 +31,8 @@ def test_write_command_유효성검사_LBA오류(test_address, mock_ssd):
     mock_ssd.write.assert_not_called()
 
 
-@pytest.mark.parametrize('test_value', ['0xHIJKLMNO', '0x1234567Z', '0x123456Z8', '0x12345Z78',
-                                        '0x1234Z678', '0x123Z5678', '0x12Z45678', '0x1Z345678',
-                                        '0xZ2345678'])
+@pytest.mark.parametrize('test_value', ['0x1234567Z', '0x123456Z8', '0x12345Z78', '0x1234Z678',
+                                        '0x123Z5678', '0x12Z45678', '0x1Z345678', '0xZ2345678'])
 def test_write_command_유효성검사_Value오류(test_value, mock_ssd):
     test_address = 0
 
