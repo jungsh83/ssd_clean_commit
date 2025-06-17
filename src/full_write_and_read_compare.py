61a794f972fae8bfc90ab2131ec8a40ab22f50a2
jws <jws2920@naver.com>
2025-06-17 15:38:43 +0900

    [feature] 요구사항에 따라 주소 5개마다 다른 value를 사용하기 위한 내장 함수 generate_test_value를 추가

--- src/full_write_and_read_compare.py
+++ src/full_write_and_read_compare.py
@@ -1,11 +1,12 @@
+import random
 from src.command_action import CommandAction
+
 
 class FullWriteAndReadCompare(CommandAction):
     def run(self) -> None:
-
         for i in range(25):
-            test_value = "0x12345678"
-            for addr in range(i*4, i*4+4):
+            test_value = self.generate_test_value()
+            for addr in range(i * 4, i * 4 + 4):
                 self._ssd_driver.write(addr, test_value)
                 if not self.read_compare(addr, test_value):
                     print("FAIL", end="")
@@ -14,10 +15,11 @@ class FullWriteAndReadCompare(CommandAction):
         print("PASS", end="")
         return
 
+    def generate_test_value(self):
+        return f"0x{random.randint(1111111, 4444444):08X}"
+
     def validate(self) -> bool:
         return self._arguments == []
 
     def read_compare(self, addr, test_value) -> bool:
         return self._ssd_driver.read(addr) == test_value
-
-
