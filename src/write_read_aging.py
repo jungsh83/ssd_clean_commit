31dd8f08c2e746488f6c440622e7078038931f6b
jws <jws2920@naver.com>
2025-06-17 16:41:20 +0900

    [feature] 일반화된 코드로 변경

--- src/write_read_aging.py
+++ src/write_read_aging.py
@@ -1,4 +1,7 @@
+import random
 from src.command_action import CommandAction
+
+
 class WriteReadAging(CommandAction):
     command_name = ["3_WriteReadAging", "3_"]
 
@@ -6,8 +9,19 @@ class WriteReadAging(CommandAction):
         return self._arguments == []
 
     def run(self) -> None:
+        for _ in range(200):
+            test_value = self.generate_test_value()
+            self._ssd_driver.write(0, test_value)
+            self._ssd_driver.write(99, test_value)
+
+            if self._ssd_driver.read(0) == self._ssd_driver.read(99):
+                pass
+            else:
+                print("FAIL")
+                return
+
         print("PASS")
         return
 
-
-
+    def generate_test_value(self):
+        return f"0x{random.randint(1111111, 4444444):08X}"
