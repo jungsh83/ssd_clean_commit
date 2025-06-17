920ff6cc0dc55590056af68973faf9ae972b670c
jws <jws2920@naver.com>
2025-06-17 16:35:54 +0900

    [feature] validte 함수 구현

--- src/write_read_aging.py
+++ src/write_read_aging.py
@@ -1,10 +1,13 @@
 from src.command_action import CommandAction
 class WriteReadAging(CommandAction):
     command_name = ["3_WriteReadAging", "3_"]
+
+    def validate(self) -> bool:
+        return self._arguments == []
+
     def run(self) -> None:
         print("PASS")
         return
 
-    def validate(self) -> bool:
-        return True
+
 
