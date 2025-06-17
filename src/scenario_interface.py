5b8f1015d2163219e8bb3e357b42c7376d152b38
jws <jws2920@naver.com>
2025-06-17 14:21:02 +0900

    [feature] IScenarioTest내 __init__ 함수를 추가 하였습니다.

--- src/scenario_interface.py
+++ src/scenario_interface.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class IScenarioTest(ABC):
+    def __init__(self, ssd_driver):
+        self._ssd_driver = ssd_driver
+
     @abstractmethod
     def read_compare(self, addr: int, expected: str) -> bool:
         ...
