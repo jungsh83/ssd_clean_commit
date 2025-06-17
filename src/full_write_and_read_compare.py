1f4e267d72cbdf0442cba64f6393f124c621a136
jws <jws2920@naver.com>
2025-06-17 15:29:42 +0900

    [feature] validate 구현, run의 일반화과정을 포함한 코드 추가

--- src/full_write_and_read_compare.py
+++ src/full_write_and_read_compare.py
@@ -2,9 +2,22 @@ from src.command_action import CommandAction
 
 class FullWriteAndReadCompare(CommandAction):
     def run(self) -> None:
+
+        for i in range(25):
+            test_value = "0x12345678"
+            for addr in range(i*4, i*4+4):
+                self._ssd_driver.write(addr, test_value)
+                if not self.read_compare(addr, test_value):
+                    print("FAIL", end="")
+                    return
+
         print("PASS", end="")
         return
 
     def validate(self) -> bool:
         return self._arguments == []
+
+    def read_compare(self, addr, test_value) -> bool:
+        return self._ssd_driver.read(addr) == test_value
+
 
