499fd92eccfe78a72467a72ce267b6a7141d8e23
jws <jws2920@naver.com>
2025-06-18 08:55:36 +0900

    [feature] test 수행 성공시 read/write 수행 횟수 점검

--- tests/test_write_read_aging.py
+++ tests/test_write_read_aging.py
@@ -41,6 +41,28 @@ def test_수행_성공(mocker: MockerFixture):
     # assert
     assert out == "PASS\n"
 
+def test_수행_성공시_read_write_횟수_점검(mocker: MockerFixture):
+    data_dict = {}
+
+    def read(addr):
+        return data_dict.get(addr, "0x00000000")
+
+    def write(addr, value):
+        data_dict[addr] = value
+
+    # arrange
+    ssd_driver = mocker.Mock()
+    ssd_driver.read.side_effect = read
+    ssd_driver.write.side_effect = write
+    sut = WriteReadAging(ssd_driver)
+
+    # act
+    out = catch_run_stdout(sut)
+
+    # assert
+    assert ssd_driver.write.call_count == 400
+    assert ssd_driver.read.call_count == 400
+
 
 def test_수행_실패(mocker: MockerFixture):
     data_dict = {}
