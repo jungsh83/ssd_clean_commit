e6d06b871898e5227138e9b855198da431b4b810
jihun75.kim <gnswlrla0218@naver.com>
2025-06-18 12:32:17 +0900

    [Refactor] Param 개수 초과하는 Test Case 추가 및 pytest.fixutre 이용해서 refactoring

--- tests/test_full_write_command.py
+++ tests/test_full_write_command.py
@@ -4,8 +4,12 @@ from src.full_write_command import FullWriteCommand
 from src.ssd import VirtualSSD
 
 
-def test_full_write_command_성공(mocker):
-    mock_ssd = mocker.Mock(spec=VirtualSSD)
+@pytest.fixture
+def mock_ssd(mocker):
+    return mocker.Mock(spec=VirtualSSD)
+
+
+def test_full_write_command_성공(mock_ssd):
     test_value = '0x12345678'
 
     full_write_cmd = FullWriteCommand(mock_ssd, test_value)
@@ -18,9 +22,7 @@ def test_full_write_command_성공(mocker):
 @pytest.mark.parametrize('test_value', ['0x1234567Z', '0x123456Z8', '0x12345Z78', '0x1234Z678',
                                         '0x123Z5678', '0x12Z45678', '0x1Z345678', '0xZ2345678',
                                         -3, 'c', 0.1])
-def test_full_write_command_유효성검사_value_에러(test_value, mocker):
-    mock_ssd = mocker.Mock(spec=VirtualSSD)
-
+def test_full_write_command_유효성검사_value_에러(test_value, mock_ssd):
     full_write_cmd = FullWriteCommand(mock_ssd, test_value)
 
     with pytest.raises(ValueError):
@@ -30,9 +32,7 @@ def test_full_write_command_유효성검사_value_에러(test_value, mocker):
     mock_ssd.write.assert_not_called()
 
 
-def test_full_write_command_유효성검사_Param개수_부족(mocker):
-    mock_ssd = mocker.Mock(spec=VirtualSSD)
-
+def test_full_write_command_유효성검사_Param개수_부족(mock_ssd):
     full_write_cmd = FullWriteCommand(mock_ssd)
 
     with pytest.raises(ValueError):
@@ -42,11 +42,11 @@ def test_full_write_command_유효성검사_Param개수_부족(mocker):
     mock_ssd.write.assert_not_called()
 
 
-def test_full_write_command_유효성검사_Param개수_초과(mocker):
-    mock_ssd = mocker.Mock(spec=VirtualSSD)
-    test_value = '0x12345678'
-    error_param = 'Error'
-
+@pytest.mark.parametrize('test_value, error_param', [('0x1234567Z', -1), ('0x123456Z8', 'c'),
+                                                     ('0x12345Z78', 1), ('0x1234Z678', 100),
+                                                     ('0x123Z5678', 0.1), ('0x12Z45678', 123),
+                                                     ('0x1Z345678', 1), ('0xZ2345678', 0)])
+def test_full_write_command_유효성검사_Param개수_초과(test_value, error_param, mock_ssd):
     full_write_cmd = FullWriteCommand(mock_ssd, test_value, error_param)
 
     with pytest.raises(ValueError):
