4436a1db0c2a68761bbbf80723684b5cb6af2c5d
sk.woo <sk.woo@samsung.com>
2025-06-18 10:47:22 +0900

    [feature] FullRead , FullReadTest작성

---
+++ src/full_read.py
@@ -0,0 +1,6 @@
+class FullRead:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
---
+++ tests/test_full_read.py
@@ -0,0 +1,11 @@
+from src.full_read import FullRead
+from src.ssd import VirtualSSD
+
+def test_fullread_prints_100_lines():
+    pass
+
+def test_fullread_first_line_matches_lba0():
+    pass
+
+def test_fullread_last_line_matches_lba99():
+    pass
