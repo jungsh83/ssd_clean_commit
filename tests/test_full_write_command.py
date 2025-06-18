7bb4ee649a8ca1510243b307a04033ea2143a435
jihun75.kim <gnswlrla0218@naver.com>
2025-06-18 10:28:59 +0900

    [Refactor] FullWrite 유효성 체크 TC type 에러와 value 에러 합치기

--- tests/test_full_write_command.py
+++ tests/test_full_write_command.py
@@ -15,22 +15,10 @@ def test_full_write_command_성공(mocker):
     assert mock_ssd.write.call_count == 100
 
 
-def test_full_write_command_유효성검사_value_type에러(mocker):
-    mock_ssd = mocker.Mock(spec=VirtualSSD)
-    test_value = 3
-
-    full_write_cmd = FullWriteCommand(mock_ssd, test_value)
-
-    with pytest.raises(ValueError):
-        full_write_cmd.run()
-
-    assert full_write_cmd.validate() is False
-    mock_ssd.write.assert_not_called()
-
-
 @pytest.mark.parametrize('test_value', ['0x1234567Z', '0x123456Z8', '0x12345Z78', '0x1234Z678',
-                                        '0x123Z5678', '0x12Z45678', '0x1Z345678', '0xZ2345678'])
-def test_full_write_command_유효성검사_value_값에러(test_value, mocker):
+                                        '0x123Z5678', '0x12Z45678', '0x1Z345678', '0xZ2345678',
+                                        -3, 'c', 0.1])
+def test_full_write_command_유효성검사_value_에러(test_value, mocker):
     mock_ssd = mocker.Mock(spec=VirtualSSD)
 
     full_write_cmd = FullWriteCommand(mock_ssd, test_value)
