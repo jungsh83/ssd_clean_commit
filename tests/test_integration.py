8b4e3b10704f8d660afa26d3b85cf184fb6a3157
jungsh83 <jungsh83@gmail.com>
2025-06-18 17:05:54 +0900

    [feature] 통합테스트 케이스 분리

---
+++ tests/test_integration.py
@@ -0,0 +1,76 @@
+import builtins
+import pytest
+
+from src import shell
+
+from src.commands.command_action import CommandAction
+from src.ssd_driver import SSDDriver
+
+
+def simulate_shell(inputs, monkeypatch):
+    input_iter = iter(inputs)
+
+    def mock_input(_):
+        return next(input_iter)
+
+    monkeypatch.setattr(builtins, "input", mock_input)
+
+
+@pytest.mark.parametrize('operation, expected',
+                         [
+                             ('write 0 0x12345678', "[WRITE] Done"),
+                             ('read 1', "[READ] LBA 00 : 0x00000000"),
+                             ('fullwrite 0x87654321', ""),
+                             ('fullread', "[FULLREAD] 0 0x87654321"),
+                             ('1_FullWriteAndReadCompare', "[1_FULLWRITEANDREADCOMPARE] PASS"),
+                             ('2_PartialLBAWrite', "[2_PARTIALLBAWRITE] PASS"),
+                             ('3_WriteReadAging', "[3_WRITEREADAGING] PASS"),
+                         ])
+def test_통합테스트_정상명령어(monkeypatch, capsys, operation, expected):
+    simulate_shell([operation, 'exit'], monkeypatch)
+
+    shell.main(SSDDriver())
+
+    captured = capsys.readouterr()
+
+    assert expected in captured.out
+    assert "[EXIT]" in captured.out
+
+
+@pytest.mark.parametrize('operation, expected',
+                         [
+                             ('write 0', "[ERROR]"),
+                             ('write 0 0x12345678 1', "[ERROR]"),
+                             ('read', "[ERROR]"),
+                             ('read 0 0x12345678', "[ERROR]"),
+                             ('fullwrite', "[ERROR]"),
+                             ('fullwrite 0 0x12345678', "[ERROR]"),
+                             ('fullread 0', "[ERROR]")
+                         ])
+def test_통합테스트_argument_개수틀림(monkeypatch, capsys, operation, expected):
+    simulate_shell([operation, 'exit'], monkeypatch)
+
+    shell.main(SSDDriver())
+
+    captured = capsys.readouterr()
+
+    assert expected in captured.out
+    assert "[EXIT]" in captured.out
+
+
+@pytest.mark.parametrize('operation, expected',
+                         [
+                             ('write 0 0x1234567', "[ERROR]"),
+                             ('write c 0x1234567', "[ERROR]"),
+                             ('read a', "[ERROR]"),
+                             ('fullwrite 0x1234567', "[ERROR]"),
+                         ])
+def test_통합테스트_argument_형식틀림(monkeypatch, capsys, operation, expected):
+    simulate_shell([operation, 'exit'], monkeypatch)
+
+    shell.main(SSDDriver())
+
+    captured = capsys.readouterr()
+
+    assert expected in captured.out
+    assert "[EXIT]" in captured.out
