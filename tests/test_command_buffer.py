abac9026638b679df617fb965c2acf7d1b85d036
jungsh83 <jungsh83@gmail.com>
2025-06-19 17:53:49 +0900

    [refactor] test case에서 initialize 호출하는 부분을 pytest.fixture로 이동

--- tests/test_command_buffer.py
+++ tests/test_command_buffer.py
@@ -8,12 +8,11 @@ from src.command_buffer import CommandBuffer, Command, CommandBufferException
 @pytest.fixture
 def command_buffer():
     command_buffer = CommandBuffer()
+    command_buffer.initialize()
     return command_buffer
 
 
 def test_initialize_성공(command_buffer):
-    command_buffer.initialize()
-
     assert command_buffer.command_buffers == [Command(order=1, command_type='I', lba=-1, value='', size=-1),
                                               Command(order=2, command_type='I', lba=-1, value='', size=-1),
                                               Command(order=3, command_type='I', lba=-1, value='', size=-1),
@@ -37,7 +36,6 @@ def test_command_buffers_객체_생성_성공_파일_없는_상태():
 
 
 def test_command_buffers_객체_생성_성공_initialize_없는_상태(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='E', lba=3, size=1))
 
     new_command_buffer = CommandBuffer()
@@ -50,7 +48,6 @@ def test_command_buffers_객체_생성_성공_initialize_없는_상태(command_b
 
 
 def test_command_buffers_객체_생성_성공(command_buffer):
-    command_buffer.initialize()
     assert command_buffer.command_buffers == [Command(order=1, command_type='I', lba=-1, value='', size=-1),
                                               Command(order=2, command_type='I', lba=-1, value='', size=-1),
                                               Command(order=3, command_type='I', lba=-1, value='', size=-1),
@@ -59,19 +56,16 @@ def test_command_buffers_객체_생성_성공(command_buffer):
 
 
 def test_fast_read_W에_값이_있을_때(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
     assert command_buffer.fast_read(3) == '0x00000001'
 
 
 def test_fast_Read_E에_값이_있을_때(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='E', lba=3, size=1))
     assert command_buffer.fast_read(3) == '0x00000000'
 
 
 def test_fast_Read_값이_없을_때(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='E', lba=3, size=1))
     assert command_buffer.fast_read(4) is None
 
@@ -91,7 +85,6 @@ def test_command_buffers_fast_read_성공_파일_없는_상태():
 
 
 def test_버퍼에_빈_값이_존재할_때_append_성공(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
     assert command_buffer.read_all() == [Command(order=1, command_type='W', lba=3, value='0x00000001', size=-1),
                                          Command(order=2, command_type='I', lba=-1, value='', size=-1),
@@ -101,7 +94,6 @@ def test_버퍼에_빈_값이_존재할_때_append_성공(command_buffer):
 
 
 def test_버퍼에_빈_값이_없을_때_failed(command_buffer):
-    command_buffer.initialize()
     command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
     command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
     command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
@@ -110,3 +102,4 @@ def test_버퍼에_빈_값이_없을_때_failed(command_buffer):
 
     with pytest.raises(CommandBufferException):
         command_buffer.append(Command(command_type='W', lba=3, value='0x00000001'))
+
