4ba1844f55300b19f4e5f87829b44a9d77285546
sse <sse4682@naver.com>
2025-06-19 18:06:43 +0900

    [feature] ssd_main 함수 및 unit test 코드 작성

--- ssd.py
+++ ssd.py
@@ -1,30 +1,41 @@
-import os
 import sys
 from src.ssd_file_manager import SSDFileManager
+from src.ssd_commands.ssd_command_action import InvalidArgumentException
+from src.command_buffer import CommandBuffer
+from src.ssd_commands.ssd_read import ReadCommand
 
+# Command 구현 전에 test 구동을 위해 임시로 None으로 처리해놨습니다
+WriteCommand = None
+EraseCommand = None
+FlushCommand = None
+
+
+SSD_COMMANDS = {
+    'R': ReadCommand,
+    'W': WriteCommand,
+    'E': EraseCommand,
+    'F': FlushCommand,
+}
 
 def main(args: list[str]):
-    ssd = SSDFileManager()
+    if not args:
+        SSDFileManager().error()
+        return
 
-    if len(args) == 2 and args[0] == ssd.COMMAND_READ:
-        lba_str = args[1]
-        if not lba_str.isdigit():
-            ssd.error()
-            return
-        lba = int(lba_str)
-        ssd.read(lba)
+    command_str = args[0]
+    command_args = args[1:]
 
-    elif len(args) == 3 and args[0] == ssd.COMMAND_WRITE:
-        lba_str, value = args[1], args[2]
-        if not lba_str.isdigit():
-            ssd.error()
-            return
-        lba = int(lba_str)
-        ssd.write(lba, value)
+    CommandClass = SSD_COMMANDS.get(command_str)
+    if not CommandClass:
+        SSDFileManager().error()
+        return
 
-    else:
-        ssd.error()
+    command = CommandClass(SSDFileManager(), CommandBuffer(), *command_args)
 
+    try:
+        command.run()
+    except InvalidArgumentException:
+        pass
 
 if __name__ == "__main__":
     main(sys.argv[1:])
