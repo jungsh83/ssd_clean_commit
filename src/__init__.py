d68f2a542109d175afe454d8b419900f1478ebc4
jihun75.kim <gnswlrla0218@naver.com>
2025-06-19 14:01:37 +0900

    [Feature] shell에서 실행 위해 __init__ 추가

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ from src.commands.write import WriteCommand
 from src.commands.full_read import FullReadCommand
 from src.commands.full_write import FullWriteCommand
 from src.commands.exit import ExitCommand
+from src.commands.erase_range import EraseRangeCommand
 
 # 테스트 시나리오
 from src.commands.full_write_and_read_compare import FullWriteAndReadCompareCommand
