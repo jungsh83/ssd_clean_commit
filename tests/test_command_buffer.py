c9584d8dc678e9e634f0c5decf7b263d2a353f2c
jungsh83 <jungsh83@gmail.com>
2025-06-19 16:36:29 +0900

    [feature] test_command_buffers_fast_read_성공_파일_없는_상태 테스트 케이스만 추가 skip 처리

--- tests/test_command_buffer.py
+++ tests/test_command_buffer.py
@@ -75,6 +75,20 @@ def test_fast_Read_값이_없을_때(command_buffer):
     command_buffer.append(Command(command_type='E', lba=3, size=1))
     assert command_buffer.fast_read(4) is None
 
+@pytest.mark.skip
+def test_command_buffers_fast_read_성공_파일_없는_상태():
+    files_in_dir = [file for file in Path("../buffer").iterdir() if file.is_file()]
+    for file in files_in_dir:
+        file.unlink(missing_ok=True)
+
+    command_buffer = CommandBuffer()
+
+    assert command_buffer.command_buffers == [Command(order=1, command_type='I', lba=-1, value='', size=-1),
+                                              Command(order=2, command_type='I', lba=-1, value='', size=-1),
+                                              Command(order=3, command_type='I', lba=-1, value='', size=-1),
+                                              Command(order=4, command_type='I', lba=-1, value='', size=-1),
+                                              Command(order=5, command_type='I', lba=-1, value='', size=-1)]
+
 
 def test_버퍼에_빈_값이_존재할_때_append_성공(command_buffer):
     command_buffer.initialize()
