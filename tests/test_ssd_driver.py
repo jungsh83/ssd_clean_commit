8cd6642a2d5e3e72bc94ab1ed2aa69491da73543
jws <jws2920@naver.com>
2025-06-19 10:59:22 +0900

    [Refactor] Mocking을 사용하여 test가 수행되도록 변경

--- tests/test_ssd_driver.py
+++ tests/test_ssd_driver.py
@@ -5,11 +5,31 @@ from pytest_mock import MockerFixture
 from src.ssd_driver import SSDDriver, ReadException, WriteException
 
 
+data_dict = {}
+
+def mk_read(lba):
+    if not (0 <= lba < 100):
+        return "ERROR"
+    else:
+        return data_dict.get(lba, "0x00000000")
+
+def mk_write(lba, value):
+    if not (0 <= lba < 100):
+        data_dict[lba] = "ERROR"
+    else:
+        data_dict[lba] = value
+
 @pytest.fixture
-def ssd_driver():
+def ssd_driver(mocker: MockerFixture):
     # initialize
     (Path(__file__).parent.parent / "data/ssd_nand.txt").unlink(missing_ok=True)
     (Path(__file__).parent.parent / "data/ssd_output.txt").unlink(missing_ok=True)
+
+    read_mock_method = mocker.patch("src.ssd_driver.SSDDriver.read_ssd")
+    write_mock_method = mocker.patch("src.ssd_driver.SSDDriver.write_ssd")
+
+    read_mock_method.side_effect = mk_read
+    write_mock_method.side_effect = mk_write
 
     ssd_driver = SSDDriver()
     return ssd_driver
