0d1051279f8072ae7b51b77f4a912f392c00f414
songju <songjuna96@daum.net>
2025-06-23 09:39:06 +0900

    [feature] SSD Erase unit test 추가

--- tests/test_ssd_erase.py
+++ tests/test_ssd_erase.py
@@ -1,4 +1,15 @@
 import pytest
+from src.ssd_commands.ssd_command_action import SSDCommand, InvalidArgumentException
+from src.ssd_commands.ssd_erase_command import SSDCommandErase
+from src.ssd_file_manager import SSDFileManager
+from src.command_buffer.command_buffer_handler import CommandBufferHandler
+
+
+@pytest.fixture
+def get_filemanage_command_handler(mocker):
+    file_manager = SSDFileManager()
+    command_buffer_handler = CommandBufferHandler()
+    return file_manager, command_buffer_handler
 
 
 @pytest.mark.parametrize("lba, size, expected_erase_start, expected_erase_end", [
@@ -6,8 +17,16 @@ import pytest
     (5, 1, 5, 5),
     (5, 10, 5, 14),
 ])
-def test_erase_성공(lba, size, expected_erase_start, expected_erase_end):
-    ...
+def test_erase_성공(get_filemanage_command_handler, lba, size, expected_erase_start, expected_erase_end):
+    file_manager, command_buffer_handler = get_filemanage_command_handler
+    cmd = SSDCommandErase(file_manager, command_buffer_handler, str(lba), str(size))
+
+    cmd.run()
+
+    # assert
+    data = cmd._ssd_file_manager._load_nand()
+    for i in range(expected_erase_start, expected_erase_end + 1):
+        assert data[i] == cmd._ssd_file_manager.DEFAULT_VAL
 
 
 @pytest.mark.parametrize("lba, size", [
@@ -18,5 +37,9 @@ def test_erase_성공(lba, size, expected_erase_start, expected_erase_end):
     ("가나다", 10),
     (5, "가다")
 ])
-def test_erase_파라미터_유효성오류(lba, size):
-    ...
+def test_erase_파라미터_유효성오류(get_filemanage_command_handler, lba, size):
+    file_manager, command_buffer_handler = get_filemanage_command_handler
+    cmd = SSDCommandErase(file_manager, command_buffer_handler, str(lba), str(size))
+
+    with pytest.raises(InvalidArgumentException):
+        cmd.run()
