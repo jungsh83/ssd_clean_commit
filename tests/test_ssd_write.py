c4bc9356d32ecb72799e97ea9317bf0db2a7c6d9
jws <jws2920@naver.com>
2025-06-23 09:14:52 +0900

    [Refactor] 변경된 폴더구조에 따라 Test코드의 dependency 경로 변경

--- tests/test_ssd_write.py
+++ tests/test_ssd_write.py
@@ -2,7 +2,8 @@ import pytest
 from pytest_mock import MockFixture
 
 from src.ssd_file_manager import SSDFileManager
-from src.command_buffer import CommandBuffer, Command
+from src.command_buffer.command_buffer_handler import CommandBufferHandler
+from src.command_buffer.command_buffer_data import CommandBufferData
 from src.ssd_commands.ssd_write import WriteCommandAction
 
 
@@ -13,31 +14,31 @@ def ssd_file_manager():
 
 @pytest.fixture
 def command_buffer_without_flush(mocker: MockFixture):
-    mocker.patch("src.command_buffer.CommandBuffer.read_all").side_effect = mk_read_all
-    mocker.patch("src.command_buffer.CommandBuffer.append").return_value = None
-    mocker.patch("src.command_buffer.CommandBuffer.is_empty_buffer_slot_existing").return_value = True
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.read_all").side_effect = mk_read_all
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.append").return_value = None
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.is_buffer_available").return_value = True
 
-    obj = CommandBuffer()
+    obj = CommandBufferHandler()
     return obj
 
 
 @pytest.fixture
 def command_buffer_with_flush(mocker: MockFixture):
-    mocker.patch("src.command_buffer.CommandBuffer.read_all").side_effect = mk_read_all
-    mocker.patch("src.command_buffer.CommandBuffer.append").return_value = None
-    mocker.patch("src.command_buffer.CommandBuffer.is_empty_buffer_slot_existing").return_value = False
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.read_all").side_effect = mk_read_all
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.append").return_value = None
+    mocker.patch("src.command_buffer.command_buffer_handler.CommandBufferHandler.is_buffer_available").return_value = False
 
-    obj = CommandBuffer()
+    obj = CommandBufferHandler()
     return obj
 
 
 def mk_read_all():
     return [
-        Command(order=1, command_type="W", lba=0, value="0x00000000"),
-        Command(order=1, command_type="W", lba=1, value="0x00000001"),
-        Command(order=1, command_type="W", lba=2, value="0x00000002"),
-        Command(order=1, command_type="W", lba=3, value="0x00000003"),
-        Command(order=1, command_type="W", lba=4, value="0x00000004"),
+        CommandBufferData(order=1, command_type="W", lba=0, value="0x00000000"),
+        CommandBufferData(order=1, command_type="W", lba=1, value="0x00000001"),
+        CommandBufferData(order=1, command_type="W", lba=2, value="0x00000002"),
+        CommandBufferData(order=1, command_type="W", lba=3, value="0x00000003"),
+        CommandBufferData(order=1, command_type="W", lba=4, value="0x00000004"),
     ]
 
 
@@ -78,4 +79,4 @@ def test_run_성공_without_flush(ssd_file_manager, command_buffer_without_flush
     [("0", "0x00000001"), ("99", "0x00000099")]
 )
 def test_run_성공_with_flush(ssd_file_manager, command_buffer_with_flush, lba, value):
-    assert WriteCommandAction(ssd_file_manager, CommandBuffer(), lba, value).run() == "PASS"
+    assert WriteCommandAction(ssd_file_manager, CommandBufferHandler(), lba, value).run() == "PASS"
