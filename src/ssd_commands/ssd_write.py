fdf63095712ce585dcfb84b7b393f38079c6342e
jws <jws2920@naver.com>
2025-06-24 10:31:10 +0900

    [Refactor] 중복 Import문 제거

--- src/ssd_commands/ssd_write.py
+++ src/ssd_commands/ssd_write.py
@@ -1,12 +1,10 @@
 from src.ssd_commands import validate_lba, validate_value
-from src.ssd_file_manager import SSDFileManager
-from src.command_buffer.command_buffer_handler import CommandBufferHandler
 from src.command_buffer.command_buffer_data import CommandBufferData, WRITE, ERASE
 from src.ssd_commands.ssd_command import SSDCommand
 
 
 class WriteSSDCommand(SSDCommand):
-    def __init__(self, ssd_file_manager: SSDFileManager, command_buffer: CommandBufferHandler, *args):
+    def __init__(self, ssd_file_manager, command_buffer, *args):
         super().__init__(ssd_file_manager, command_buffer, *args)
 
         self.lba: int|None = None
