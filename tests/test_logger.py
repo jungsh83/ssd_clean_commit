937ea1c39d334bf11a24b139106c6cb2b0623075
songju <songjuna96@daum.net>
2025-06-24 10:22:23 +0900

    [feature] logger.py의 테스트 커버리지를 100%로 만들기 위해 test_logger.py에 유닛테스트를 추가하였습니다.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -45,3 +45,35 @@ def test_logger_파일생성되는지_각파일이10kB넘지않는지(fixed_log_
         assert zip_file.stat().st_size <= max_size_with_margin, f"{zip_file.name} 크기 초과"
 
     assert latest_log.stat().st_size <= max_size_with_margin, "latest.log 너무 큼 (rotate 안 됨)"
+
+
+def test_logger_log_잘나오는지(fixed_log_dir):
+    logger = LoggerSingleton.get_logger()
+
+    logger.info("info")
+    logger.debug("debug")
+    logger.error("error")
+
+    latest_log = fixed_log_dir / "latest.log"
+
+    assert latest_log.exists(), "latest.log 파일이 존재하지 않음"
+
+    log_content = latest_log.read_text(encoding="utf-8")
+
+    assert "INFO" in log_content, "'INFO' 로그가 기록되지 않음"
+    assert "DEBUG" in log_content, "'DEBUG' 로그가 기록되지 않음"
+    assert "ERROR" in log_content, "'ERROR' 로그가 기록되지 않음"
+
+
+def test_logger_builtin_print_패치되는지(fixed_log_dir):
+    logger = LoggerSingleton.get_logger()
+
+    print("builtin_print_patched")
+
+    latest_log = fixed_log_dir / "latest.log"
+
+    assert latest_log.exists(), "latest.log 파일이 존재하지 않음"
+
+    log_content = latest_log.read_text(encoding="utf-8")
+
+    assert "builtin_print_patched" in log_content, "bulit-in print 함수의 출력이 패치되지 않음"
