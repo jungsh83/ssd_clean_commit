53d188dcf9e7f989fbefa141b6fdf5f14c669e89
jws <jws2920@naver.com>
2025-06-24 10:33:58 +0900

    [Refactor] 불필요 주석 삭제

--- src/ssd_commands/ssd_write.py
+++ src/ssd_commands/ssd_write.py
@@ -7,8 +7,8 @@ class WriteSSDCommand(SSDCommand):
     def __init__(self, ssd_file_manager, command_buffer, *args):
         super().__init__(ssd_file_manager, command_buffer, *args)
 
-        self.lba: int|None = None
-        self.value: str|None = None
+        self.lba: int | None = None
+        self.value: str | None = None
 
     def validate(self) -> bool:
         if len(self._arguments) != 2:
@@ -30,11 +30,9 @@ class WriteSSDCommand(SSDCommand):
             self._ssd_file_manager.error()
             return "FAIL"
 
-        # Buffer가 가득차 있다면 flush 수행
         if not self._command_buffer.is_buffer_available():
             self.do_flush()
 
-        # Command를 buffer에 추가
         self.append_command_into_command_buffer()
 
         return "PASS"
@@ -44,17 +42,13 @@ class WriteSSDCommand(SSDCommand):
             CommandBufferData.create_write_command(lba=self.lba, value=self.value)
         )
 
-
-
     def do_flush(self):
-        # command들을 fileManager를 통해 수행
         for command in self._command_buffer.command_buffers:
             if command.command_type == WRITE:
                 self._ssd_file_manager.write(command.lba, command.value)
             elif command.command_type == ERASE:
                 self._ssd_file_manager.erase(command.lba, command.size)
 
-        # command buffer를 초기화
         self._command_buffer.initialize()
 
         return
